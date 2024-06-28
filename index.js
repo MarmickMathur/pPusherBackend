729bb31c2581f9a9f42546ffd18bd48f5a41c6b0
Shrikrishna Mathur <22je0547@iitism.ac.in>
2024-06-29 00:03:50 +0530

    keep alive added

--- index.js
+++ index.js
@@ -4,6 +4,31 @@ const cors = require("cors");
 
 const app = express();
 
+// Keep-alive functionality
+const keepServerActive = () => {
+  fetch("https://nodemailerapi-0cfa.onrender.com/email/keep-alive", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({}), // Sending an empty JSON object as body
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("Server kept alive:", data);
+    })
+    .catch((error) => {
+      console.error("Error keeping server alive:", error);
+    });
+};
+
+app.post("/keep-alive", (req, res) => {
+  res.status(200).send({ message: "Server is alive" });
+});
+
+// Send a POST request every 3 seconds (3000 milliseconds)
+setInterval(keepServerActive, 3000);
+
 app.use(cors());
 
 app.use(express.json());
